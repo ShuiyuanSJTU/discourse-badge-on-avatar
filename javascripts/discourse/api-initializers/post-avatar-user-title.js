7c63bed616bd3f0cedad1ab2c95eebed944eb6c3
pangbo13 <373108669@qq.com>
2022-07-16 14:00:08 +0800

    disable badge on topic-participant

--- javascripts/discourse/api-initializers/post-avatar-user-title.js
+++ javascripts/discourse/api-initializers/post-avatar-user-title.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import User from "discourse/models/user";
 import Badge from "discourse/models/badge";
 import { apiInitializer } from "discourse/lib/api";
@@ -6,10 +7,11 @@ export default apiInitializer("0.11.1", (api) => {
   const badgeInfoMap = new Map();
   let badgeInfoReady = false;
   const rerenderList = [];
+  const badge_groups_to_show = settings.badge_groups_to_show ?? [];
 
   Badge.findAll().then(badge_list => {
     badge_list.filter((badge) => {
-      return badge.allow_title && settings.badge_groups_to_show.includes(badge.badge_grouping?.name) && badge.image_url;
+      return badge.allow_title && badge_groups_to_show.includes(badge.badge_grouping?.name) && badge.image_url;
     }).forEach((badge) => {
       badgeInfoMap.set(badge.name, badge.image_url);
     });
@@ -17,18 +19,23 @@ export default apiInitializer("0.11.1", (api) => {
     rerenderList.forEach((widget) => widget.scheduleRerender());
   });
 
-  const reopenWidgetList = ["post-avatar","topic-participant"];
+  const reopenWidgetList = ["post-avatar"];
+  if (settings.show_on_topic_participant) {
+    reopenWidgetList.push("topic-participant");
+  }
+
   reopenWidgetList.forEach( function (widgetName) {
     return api.reopenWidget(widgetName, {
       getUserTitle(attrs) {
         if (attrs.user_title !== undefined) {
           return attrs.user_title;
         } else if (attrs.username !== undefined) {
-          const widget = this;
-          User.findByUsername(attrs.username).then((user) => {
-            attrs.user_title = user.title;
-            widget.scheduleRerender();
-          });
+          // 保险起见，先去掉这部分
+          // const widget = this;
+          // User.findByUsername(attrs.username).then((user) => {
+          //   attrs.user_title = user.title;
+          //   widget.scheduleRerender();
+          // });
         }
       },
       getTitleImgUrl(attrs) {
