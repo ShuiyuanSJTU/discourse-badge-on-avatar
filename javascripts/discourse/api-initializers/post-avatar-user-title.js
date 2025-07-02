1f8e0f392f44d293e29c9c3ddfec175f4a0f30d6
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2025-07-03 00:17:15 +0800

    chore: linting

--- javascripts/discourse/api-initializers/post-avatar-user-title.js
+++ javascripts/discourse/api-initializers/post-avatar-user-title.js
@@ -1,7 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-import User from "discourse/models/user";
-import Badge from "discourse/models/badge";
 import { apiInitializer } from "discourse/lib/api";
+import Badge from "discourse/models/badge";
 
 export default apiInitializer("0.11.1", (api) => {
   const badgeInfoMap = new Map();
@@ -9,12 +7,18 @@ export default apiInitializer("0.11.1", (api) => {
   const rerenderList = [];
   const badge_groups_to_show = settings.badge_groups_to_show ?? [];
 
-  Badge.findAll().then(badge_list => {
-    badge_list.filter((badge) => {
-      return badge.allow_title && badge_groups_to_show.includes(badge.badge_grouping?.name) && badge.image_url;
-    }).forEach((badge) => {
-      badgeInfoMap.set(badge.name, badge.image_url);
-    });
+  Badge.findAll().then((badge_list) => {
+    badge_list
+      .filter((badge) => {
+        return (
+          badge.allow_title &&
+          badge_groups_to_show.includes(badge.badge_grouping?.name) &&
+          badge.image_url
+        );
+      })
+      .forEach((badge) => {
+        badgeInfoMap.set(badge.name, badge.image_url);
+      });
     badgeInfoReady = true;
     rerenderList.forEach((widget) => widget.scheduleRerender());
   });
@@ -24,7 +28,7 @@ export default apiInitializer("0.11.1", (api) => {
     reopenWidgetList.push("topic-participant");
   }
 
-  reopenWidgetList.forEach( function (widgetName) {
+  reopenWidgetList.forEach(function (widgetName) {
     return api.reopenWidget(widgetName, {
       getUserTitle(attrs) {
         if (attrs.user_title !== undefined) {
@@ -48,10 +52,14 @@ export default apiInitializer("0.11.1", (api) => {
         if (!badgeInfoReady) {
           rerenderList.push(this);
         }
-        if ((!(attrs.flair_url || attrs.flair_bg_color) || settings.override_group_flair) && this.hasTitleImg(attrs)) {
+        if (
+          (!(attrs.flair_url || attrs.flair_bg_color) ||
+            settings.override_group_flair) &&
+          this.hasTitleImg(attrs)
+        ) {
           attrs.flair_name = this.getUserTitle(attrs);
           attrs.flair_url = this.getTitleImgUrl(attrs);
-          if (attrs.flair_url) { 
+          if (attrs.flair_url) {
             attrs.flair_group_id = -1;
           }
           let result = this._super(attrs, ...args);
