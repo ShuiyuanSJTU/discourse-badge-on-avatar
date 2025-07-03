bd94b516e09b16e7c7cb8a23518528180b7c35f5
pangbo13 <51732678+pangbo13@users.noreply.github.com>
2025-07-03 20:42:24 +0800

    fix: transform posts from all sources

--- javascripts/discourse/api-initializers/post-avatar-user-title.js
+++ javascripts/discourse/api-initializers/post-avatar-user-title.js
@@ -28,41 +28,62 @@ const cachedBadgeInfo = {
   },
 };
 
+function transformPost(post) {
+  cachedBadgeInfo.fetchBadgeIcon().then((badgeIcon) => {
+    if (!post.user_title) {
+      return;
+    }
+    const badgeUrl = badgeIcon.get(post.user_title);
+    // To display badge on flair:
+    // Must have `badgeUrl` AND
+    // `settings.override_group_flair` is true OR no original flair
+    if (
+      badgeUrl &&
+      (settings.override_group_flair || !post.flair_group_id || !post.flair_url)
+    ) {
+      post.flair_name = post.user_title;
+      post.flair_url = badgeUrl;
+      post.flair_group_id = -1; // flair_group_id must be true to render flair
+    }
+  });
+}
+
 export default apiInitializer("0.11.1", (api) => {
   withSilencedDeprecations("discourse.post-stream-widget-overrides", () =>
     widgetImplementation(api)
   );
 
+  if (settings.block_topic_render_until_badge_loaded) {
+    api.modifyClass(
+      "route:topic",
+      (Superclass) =>
+        class extends Superclass {
+          afterModel(model) {
+            super.afterModel?.(model);
+            // the transition will pause until the promise resolves
+            return cachedBadgeInfo.fetchBadgeIcon();
+          }
+        }
+    );
+  }
+
   // New glimmer post stream
   api.addTrackedPostProperties("flair_url", "flair_group_id");
 
   api.modifyClass(
-    "controller:topic",
+    "model:post-stream",
     (Superclass) =>
       class extends Superclass {
-        init() {
-          super.init(...arguments);
-          cachedBadgeInfo.fetchBadgeIcon().then((badgeIcon) => {
-            this.get("model.postStream.posts").forEach((post) => {
-              if (!post.user_title) {
-                return;
-              }
-              const badgeUrl = badgeIcon.get(post.user_title);
-              // To display badge on flair:
-              // Must have `badgeUrl` AND
-              // `settings.override_group_flair` is true OR no original flair
-              if (
-                badgeUrl &&
-                (settings.override_group_flair ||
-                  !post.flair_group_id ||
-                  !post.flair_url)
-              ) {
-                post.flair_name = post.user_title;
-                post.flair_url = badgeUrl;
-                post.flair_group_id = -1; // flair_group_id must be true to render flair
-              }
-            });
-          });
+        appendPost(_post) {
+          const post = super.appendPost?.(_post);
+          transformPost(post);
+          return post;
+        }
+
+        prependPost(_post) {
+          const post = super.prependPost?.(_post);
+          transformPost(post);
+          return post;
         }
       }
   );
